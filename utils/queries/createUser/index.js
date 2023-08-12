26f201d1df95760f12df9aaefacb0a8314418b9b
avicity7 <karlorjalo@gmail.com>
2023-08-12 10:55:49 +0800

    fix: Use await

--- utils/queries/createUser/index.js
+++ utils/queries/createUser/index.js
@@ -1,39 +1,7 @@
 var AWS = require('aws-sdk');
 
-const putItem = async(ddb, params, callback) => {
-  ddb.putItem(params, function(err, data) {
-    console.log('putItem finished')
-    if (err) {
-      console.log('There has been a problem with your fetch operation: ' + err.message);
-      var responseCode = 500;
-
-      let response = {
-          statusCode: responseCode,
-          body: JSON.stringify(err)
-      }
-
-      console.log("response: " + JSON.stringify(response))
-      callback(null, response);
-    } else {
-      console.log('success')
-      let response = {
-        statusCode: 200,
-        body: JSON.stringify({"result": "Item added successfully"}),
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type,user",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        }
-      }
-      console.log("response: " + JSON.stringify(response))
-      callback(null, response);
-    }
-  });
-}
-
 exports.handler = async function(event, context, callback){
     if ((event.fullname && event.email && event.password) || (event.queryStringParameters && event.queryStringParameters.fullname && event.queryStringParameters.email && event.queryStringParameters.passsword)) {
-      console.log("here")
       if (event.fullname && event.email && event.password) {
           var fullname = event.fullname;
           var email = event.email; 
@@ -43,6 +11,7 @@ exports.handler = async function(event, context, callback){
         var email = event.queryStringParameters.email;
         var password = event.queryStringParameters.password;
       }
+
       AWS.config.update({region: 'us-east-1'});
       var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
       var params = {
@@ -55,6 +24,34 @@ exports.handler = async function(event, context, callback){
           'user_password': {S: password}
         }
       }
-      await putItem(ddb, params, callback)
+
+      try {
+        const data = await ddb.putItem(params).promise()
+        if (data) {
+          let response = {
+            statusCode: 200,
+            body: JSON.stringify({result: "Item added sucessfully"}),
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type,user",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            }
+          }
+          console.log("response: " + JSON.stringify(response))
+          callback(null, response);
+        }
+      } catch(err) {
+        console.log(err)
+        var responseCode = 500;
+
+        let response = {
+            statusCode: responseCode,
+            body: JSON.stringify(err)
+        }
+
+        console.log("response: " + JSON.stringify(response))
+        callback(null, response);
+      }
+      
     }
 }
