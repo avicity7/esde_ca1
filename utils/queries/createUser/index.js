11239cd74d7f6279fa96aa3065dd792491820ba3
avicity7 <karlorjalo@gmail.com>
2023-08-12 16:22:15 +0800

    fix: Add Content-Type

--- utils/queries/createUser/index.js
+++ utils/queries/createUser/index.js
@@ -48,6 +48,7 @@ exports.handler = async function(event, context, callback){
             statusCode: 200,
             body: JSON.stringify(result),
             headers: {
+                'Content-Type': 'application/json',
                 "Access-Control-Allow-Headers" : "Content-Type,user",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
