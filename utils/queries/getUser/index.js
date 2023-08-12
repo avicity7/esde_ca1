268ce38567aa59decf71edea2462b86e39d84ada
avicity7 <karlorjalo@gmail.com>
2023-08-12 09:56:34 +0800

    fix: Remove user password from query

--- utils/queries/getUser/index.js
+++ utils/queries/getUser/index.js
@@ -10,23 +10,32 @@ exports.handler = async function(event, context, callback){
         var table_name = "users"
         var expr_attr_values = { ":user_id": user_id }
         var key_cond_expr = "user_id=:user_id"
-        var proj_expr = "user_id, email, fullname, role_id, user_password"
+        var proj_expr = "user_id, email, fullname, role_id"
         await dynamodbQuery(region, table_name,expr_attr_values,key_cond_expr,proj_expr)
-        .then(data => {
+        .then(async(data) => {
                     console.log("Successfully got items from dynamodb.query")
-                    var responseCode = 200;
-                    var jsonResult = {'data': data.Items[0]}
-                    let response = {
-                            statusCode: responseCode,
-                            body: JSON.stringify(jsonResult),
-                            headers: {
-                                "Access-Control-Allow-Headers" : "Content-Type,user",
-                                "Access-Control-Allow-Origin": "*",
-                                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                            }
-                    }
-                    console.log("response: " + JSON.stringify(response))
-                    callback(null, response);
+                    var userResult = {'data': data.Items[0]}
+                    var region = "us-east-1"
+                    var table_name = "test"
+                    var expr_attr_values = { ":role_id": userResult.role_id }
+                    var key_cond_expr = "role_id=:role_id"
+                    var proj_expr = "role_id, role_name"
+                    await dynamodbQuery(region, table_name,expr_attr_values,key_cond_expr,proj_expr)
+                    .then(roleData => {
+                        var responseCode = 200;
+                        
+                        let response = {
+                                statusCode: responseCode,
+                                body: JSON.stringify(jsonResult),
+                                headers: {
+                                    "Access-Control-Allow-Headers" : "Content-Type,user",
+                                    "Access-Control-Allow-Origin": "*",
+                                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                                }
+                        }
+                        console.log("response: " + JSON.stringify(response))
+                        callback(null, response);
+                    })
             })
             .catch(error => {
                     console.log('There has been a problem with your fetch operation: ' + error.message);
