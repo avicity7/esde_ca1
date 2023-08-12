8e189f7245d10cbcded369f4e1da22b3f6df6e08
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-12 22:03:16 +0800

    fix: Index cors

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(morgan('combined', {
 
 app.use(morgan('combined'));
 app.use(cookieParser());
-app.use('*', cors());
+app.use(cors({credentials: true, origin: true}));
 
 // Server Settings
 const PORT = 5000;
