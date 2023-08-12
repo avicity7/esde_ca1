de0154f32b26a6c919d03c9dc2265022bf961410
avicity7 <karlorjalo@gmail.com>
2023-08-12 10:31:55 +0800

    fix: Import syntax

--- utils/queries/createUser/index.js
+++ utils/queries/createUser/index.js
@@ -1,5 +1,5 @@
 var AWS = require('aws-sdk');
-import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
 exports.handler = async function(event, context, callback){
     if ((event.fullname && event.email && event.password) || (event.queryStringParameters && event.queryStringParameters.fullname && event.queryStringParameters.email && event.queryStringParameters.passsword)) {
         if (event.fullname && event.email && event.password) {
