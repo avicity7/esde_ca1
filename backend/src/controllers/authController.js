d38340b3180cc150f566664496d6093f73212777
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-13 21:22:18 +0800

    fix: secure to true

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -29,7 +29,7 @@ exports.processLogin = (req, res, next) => {
             }; // End of data variable setup
 
             res.cookie('cookie', token, {
-              secure: process.env.NODE_ENV !== 'development',
+              secure: true,
               httpOnly: true,
               expires: dayjs().add(30, 'days').toDate(),
             });
