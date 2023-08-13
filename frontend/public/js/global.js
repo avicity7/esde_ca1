853c3a39224e8407f8d6a501f2d362d71f991bee
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-13 21:52:49 +0800

    global.js

--- frontend/public/js/global.js
+++ frontend/public/js/global.js
@@ -2,7 +2,6 @@ $('#logoutButton').on('click', function(event) {
   event.preventDefault();
   localStorage.clear();
   axios.get('http://44.194.159.42:5000/api/user/logout', {
-    withCredentials: true,
   })
       .then((response) => {
         window.location.replace('/home.html');
