0d8f4832c19c27478c706008b55c4b2afbc9ebd8
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-13 22:46:41 +0800

    fix: remove withCred

--- frontend/public/js/check_admin_permission.js
+++ frontend/public/js/check_admin_permission.js
@@ -1,6 +1,4 @@
-axios.get('http://44.194.159.42:5000/api/user/checkForPageAccess', {
-  withCredentials: true,
-})
+axios.get('http://44.194.159.42:5000/api/user/checkForPageAccess')
     .then((response) => {
       if (response !== undefined) {
         window.location.replace(window.location.origin + '/');
