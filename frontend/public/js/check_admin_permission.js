02d51ec8f6224156030c1c3ced9d006775f7fd70
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-13 22:51:27 +0800

    fix: Page access

--- frontend/public/js/check_admin_permission.js
+++ frontend/public/js/check_admin_permission.js
@@ -1,4 +1,8 @@
-axios.get('http://44.194.159.42:5000/api/user/checkForPageAccess')
+axios({
+  method: 'get',
+  url: 'http://44.194.159.42:5000/api/user/checkForPageAccess',
+  headers: {'Authorization': 'Bearer '.concat(localStorage.getItem('token'))},
+})
     .then((response) => {
       if (response !== undefined) {
         window.location.replace(window.location.origin + '/');
