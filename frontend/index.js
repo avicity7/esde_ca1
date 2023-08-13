d2b84a740a264860be1cb2185fb5e723f51e0d21
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-13 22:06:09 +0800

    fix: Go back to HTTP

--- frontend/index.js
+++ frontend/index.js
@@ -31,15 +31,19 @@ app.use(serveStatic(__dirname+'/public'));
 app.get('/', (req, res) => {
   res.sendFile('/public/home.html', {root: __dirname});
 });
-https
-    .createServer(
-        {
-          key: fs.readFileSync('./key.pem', 'utf-8'),
-          cert: fs.readFileSync('./cert.pem', 'utf-8'),
-        },
-        app)
-    .listen(port, ()=>{
-      console.log(`server is runing at port ${port}`);
-    });
+// https
+//     .createServer(
+//         {
+//           key: fs.readFileSync('./key.pem', 'utf-8'),
+//           cert: fs.readFileSync('./cert.pem', 'utf-8'),
+//         },
+//         app)
+//     .listen(port, ()=>{
+//       console.log(`server is runing at port ${port}`);
+//     });
+
+app.listen(port, 'localhost', () => {
+  console.log('Server hosted at Localhost port 5000');
+});
 
 
