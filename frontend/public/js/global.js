0513a7bb83d2c650cc785df10267bda3df5f5a9a
avicity7 <karlorjalo@gmail.com>
2023-08-13 12:19:09 +0800

    fix: withCreds

--- frontend/public/js/global.js
+++ frontend/public/js/global.js
@@ -1,9 +1,7 @@
 $('#logoutButton').on('click', function(event) {
   event.preventDefault();
   localStorage.clear();
-  axios.get('http://54.147.112.114:5000/api/user/logout', {
-    withCredentials: true,
-  })
+  axios.get('http://54.147.112.114:5000/api/user/logout')
       .then((response) => {
         window.location.replace('/home.html');
       });
