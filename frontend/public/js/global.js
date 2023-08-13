f205e2c330c37f9ddd6e9f40796388f540c1e754
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-08-13 22:35:24 +0800

    fix: Everything

--- frontend/public/js/global.js
+++ frontend/public/js/global.js
@@ -1,9 +1,5 @@
 $('#logoutButton').on('click', function(event) {
   event.preventDefault();
   localStorage.clear();
-  axios.get('http://44.194.159.42:5000/api/user/logout', {
-  })
-      .then((response) => {
-        window.location.replace('/home.html');
-      });
+  window.location.replace('/home.html');
 });
